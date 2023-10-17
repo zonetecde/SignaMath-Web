07f2271a9871d6c78147b68716fcff9b8b6c9d59
zonetecde <zonetecde@gmail.com>
2023-10-17 10:43:50 +0200

    imported svelte katex library to display math

--- src/components/ConfigurationPanel.svelte
+++ src/components/ConfigurationPanel.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
-	import MathJax from '../extensions/MathJax.svelte';
+	import Katex from 'svelte-katex';
 
-	$: equ = `$$x = \\frac{t}{3}$$`;
+	let equ = `x = \\frac{5}{x} `;
 </script>
 
 <div class="md:w-2/12 md:min-w-[200px] bg-[#c3aac5cb] md:h-full p-3 flex flex-col">
 	<p class="text-md md:text-xl">Étude des variations d'une fonction :</p>
 	<button
 		on:mousedown={() => {
-			equ = `$$x = \\frac{t}{503}$$`;
-		}}>{equ}</button
+			equ = `z = \\frac{5}{x}`;
+		}}>click me</button
 	>
+
+	<Katex>{equ}</Katex>
 </div>
