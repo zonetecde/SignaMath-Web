a690b9d16908de6d57546defd74f9eeb4bc8fed1
Rayane Staszewski <zonetecde@gmail.com>
2023-10-19 21:28:51 +0200

    + saisieMath à priori fini + event forwarding

--- src/components/ConfigurationPanel.svelte
+++ src/components/ConfigurationPanel.svelte
@@ -1,17 +1,36 @@
 <script lang="ts">
 	import SaisieMath from './SaisieMath.svelte';
 
-	let equ = `x = \\frac{5}{x} `;
+	function handleFunctionNameChanged(e: CustomEvent<any>): void {}
+
+	function handleVariableNameChanged(e: CustomEvent<any>): void {}
+
+	function handleFunctionChanged(e: CustomEvent<any>): void {}
 </script>
 
 <div class="md:w-2/12 md:min-w-[200px] bg-[#c3aac5cb] md:h-full p-3 flex flex-col">
 	<p class="text-md md:text-xl">Étude des variations d'une fonction :</p>
 
-	<div class="h-10 mt-3 flex flex-row">
-		<SaisieMath classes="text-center w-4 pl-2 rounded-l-lg" defaultValue={'f'} maxLength={1} />
-		<SaisieMath classes="w-3 pl-1" defaultValue={'('} maxLength={1} isDisabled />
-		<SaisieMath classes="w-4 pl-0.5" defaultValue={'x'} maxLength={1} />
-		<SaisieMath classes="w-7 " defaultValue={') = '} maxLength={1} isDisabled />
-		<SaisieMath classes="w-full  pl-1 rounded-r-lg" defaultValue={'x^2 - 4x'} maxLength={999} />
+	<div class="mt-3 flex flex-row items-center rounded-lg bg-white">
+		<SaisieMath
+			on:valueChanged={handleFunctionNameChanged}
+			classes="text-center w-10 text-right pl-2 rounded-l-lg"
+			value={'f'}
+			maxLength={3}
+		/>
+		<SaisieMath classes="w-3 pl-1" value={'('} maxLength={1} isDisabled />
+		<SaisieMath
+			on:valueChanged={handleVariableNameChanged}
+			classes="w-4 pl-0.5"
+			value={'x'}
+			maxLength={1}
+		/>
+		<SaisieMath classes="w-7 " value={') = '} maxLength={1} isDisabled />
+		<SaisieMath
+			on:valueChanged={handleFunctionChanged}
+			classes="w-full pl-1 rounded-r-lg"
+			value={'x^2 - 4x'}
+			maxLength={999}
+		/>
 	</div>
 </div>
--- src/components/SaisieMath.svelte
+++ src/components/SaisieMath.svelte
@@ -1,35 +1,75 @@
 <script lang="ts">
+	import { createEventDispatcher } from 'svelte';
 	import Katex from 'svelte-katex';
 
 	export let classes = '';
-	export let defaultValue = '';
+	export let value = '';
+	export let placeholder = value;
 	export let maxLength = 255;
 	export let isDisabled = false;
 	export let isEditing = false;
+
+	let ref: HTMLInputElement;
+
+	const dispatch = createEventDispatcher();
+
+	/**
+	 * Affiche la saisie en format Katex
+	 * Vérifie si la saisie est vide; dans ce cas
+	 * on laisse la saisie éditable pour avoir le placeholder
+	 */
+	const validateInput = () => {
+		if (ref.value.length !== 0) {
+			isEditing = false;
+			dispatch('valueChanged', value);
+		}
+		// else : le placeholder est affiché vu qu'il n'y a pas de texte
+		// dans l'input
+	};
 </script>
 
 {#if isEditing}
 	<input
-		class={`"h-full text-xl outline-none bg-white ${classes} "`}
+		class={`"h-full flex-grow text-xl outline-none placeholder:text-opacity-50 ${classes} "`}
 		type="text"
 		spellcheck="false"
-		bind:value={defaultValue}
+		bind:value
 		maxlength={maxLength}
 		disabled={isDisabled}
-		on:blur={() => {
-			isEditing = false;
+		{placeholder}
+		on:mouseup={() => {
+			// Permet d'auto focus l'input lorsqu'on clique sur la div Katex
+			ref.focus();
 		}}
+		on:focusin={() => {
+			// Permet de sélectionner le contenue l'input si elle ne contient
+			// qu'une variable ou un nom de fonction
+			if (maxLength <= 3) {
+				ref.setSelectionRange(0, 1);
+			}
+		}}
+		on:blur={() => {
+			validateInput();
+		}}
+		on:keydown={(e) => {
+			if (e.key === 'Enter') {
+				validateInput();
+			}
+		}}
+		bind:this={ref}
 	/>
 {:else}
 	<div
-		class={`bg-white h-full pt-2.5 ${classes} ` +
+		class={`pb-2 pt-2.5 select-none ${classes} ` +
 			(isDisabled ? 'cursor-default' : 'cursor-text hover:font-bold hover:text-blue-700')}
 		on:mousedown={() => {
-			isEditing = true;
+			if (!isDisabled) {
+				isEditing = true;
+			}
 		}}
 		role="button"
 		tabindex="0"
 	>
-		<Katex>{defaultValue}</Katex>
+		<Katex>{value}</Katex>
 	</div>
 {/if}
