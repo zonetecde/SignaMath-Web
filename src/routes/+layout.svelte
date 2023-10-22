490b670bcdf5e201521b22dde3b4dfa9fbba575a
Rayane Staszewski <zonetecde@gmail.com>
2023-10-22 23:46:10 +0200

    ajustement sur le titre en vue mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 
 <div class="h-screen w-screen">
 	<nav
-		class="bg-[#7a647c] h-10 md:h-20 flex flex-row relative items-center px-5 border-b border-gray-900 outline-b"
+		class="bg-[#7a647c] h-12 md:h-20 flex flex-row relative items-center px-5 border-b border-gray-900 outline-b"
 	>
 		<div
 			class="absolute left-0 right-0 px-40 py-1 h-full flex flex-row items-center justify-center gap-4 bg-gradient-to-r from-[#c3adc500] via-[#c3adc5] to-[#c3adc500]"
@@ -21,7 +21,7 @@
 			<!-- svelte-ignore a11y-click-events-have-key-events -->
 			<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 			<p
-				class="text-black font-bold text-3xl select-none cursor-pointer"
+				class="text-black md:font-bold text-2xl md:text-3xl select-none cursor-pointer"
 				on:click={() => window.location.reload()}
 			>
 				SignaMath
